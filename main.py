e9499f855f4b103c4c2127024309de10d1847ca3
ilydkin <ilydkin@gmail.com>
2023-03-03 18:51:59 +0600

    main() assembled

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import joblib
 import pandas as pd
 import json
 
-from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, ExtraTreesClassifier, AdaBoostClassifier
@@ -10,18 +9,22 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
 
-from sklearn import metrics
-
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV, cross_validate
+
 
 
 def main():
     print('Scikit-learn models tuning and comparison')
 
+    with open('data/grid_search_dict.json', 'r') as file:
+        grid_search_dict = json.load(file)
+
     df = pd.read_csv('data/feed.csv')
 
     X = df.drop('income', axis=1)
     y = df['income'].apply(lambda x: 1.0 if x > 0.0 else 0.0)
+
+    train, test, target, target_test = train_test_split (X, y, test_size=.2, random_state=34)
 
     models = (
         LogisticRegression(),
@@ -39,15 +42,25 @@ def main():
     best_score = .0
     best_model = None
     for model in models:
-        score = cross_val_score (model, X, y, cv=5, scoring= 'accuracy')
-        print(f'model: {type(model).__name__}, acc_mean: {score.mean():.4f}, acc_std: {score.std():.4f}')
+        GS = GridSearchCV(
+            estimator=model,
+            param_grid=grid_search_dict[type(model).__name__],
+            scoring='accuracy',
+            cv=5,
+            verbose=4
+        )
 
-        if score.mean() > best_score:
-            best_score = score.mean()
-            best_model = model
+        GS.fit(train, target)
+        print (f'model: {type(model).__name__} best score{GS.best_score_} ')
+
+        if GS.best_score_() > best_score:
+            best_score = GS.best_score_()
+            best_model = GS.best_estimator_
 
     print(f'best model: {type(best_model).__name__}, accuracy: {best_score:.4f}')
-    # joblib.dump(best_pipe, 'loan_pipe.pkl')
+
+    best_model_trained = best_model.fit(X,y)
+    joblib.dump(best_model_trained, 'best_model_trained.pkl')
 
 
 # Press the green button in the gutter to run the script.
