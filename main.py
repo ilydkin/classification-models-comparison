86077096e842603b7b398c5248d80a52ee4e42e9
ilydkin <ilydkin@gmail.com>
2023-03-04 12:30:48 +0600

    managing a git pull issue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import joblib
 import pandas as pd
+import joblib
 import json
+import logging
 
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -10,7 +11,6 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
 
 from sklearn.model_selection import train_test_split, GridSearchCV
-
 
 
 def main():
@@ -41,6 +41,10 @@ def main():
 
     best_score = .0
     best_model = None
+    best_params = None
+    df_grid_search_logs = pd.DataFrame()
+    logging.basicConfig(filename='data/info.log', encoding='utf-8', level=logging.INFO)
+
     for model in models:
         GS = GridSearchCV(
             estimator=model,
@@ -51,18 +55,24 @@ def main():
         )
 
         GS.fit(train, target)
-        print (f'model: {type(model).__name__} best score: {GS.best_score_}')
+        df_logs = pd.DataFrame(GS.cv_results_)
+        df_grid_search_logs = pd.concat([df_grid_search_logs, df_logs])
+
+        logging.info (f'model: {type(model).__name__} best score: {GS.best_score_}')
 
         if GS.best_score_ > best_score:
             best_score = GS.best_score_
             best_model = GS.best_estimator_
+            best_params = GS.best_params_
 
-    print(f'best model: {type(best_model).__name__}, accuracy: {best_score:.4f}')
+    logging.info(f'best model: {type(best_model).__name__}, '
+                 f'accuracy: {best_score:.4f}'
+                 f'best parameters: {best_params}')
 
-    best_model_trained = best_model.fit(X,y)
-    joblib.dump(best_model_trained, 'best_model_trained.pkl')
+    df_grid_search_logs.to_csv('data/grid_search_logs.csv')
+    best_model.fit(X,y)
+    joblib.dump(best_model,'data/best_model_trained.pkl')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
